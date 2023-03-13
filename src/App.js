899a88d4ee63abfdee2dc15f0b10f86a04406024
Francisco Vargas <P4nZh0p@gmail.com>
2023-03-13 18:06:10 -0300

    update final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FormHooks from './Components/FormHooks';
+import Resultado from './Components/Resultado';
 
 function App() {
+  const [estado, setEstado] = useState ({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormHooks datos={estado} setDatos={setEstado}/>
+      <Resultado data={estado}/>
     </div>
   );
 }
